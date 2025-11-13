04c36dc51fd5e94d0a816d6ed54c35c1bb8187c0
Aaron Mateo Ayala Cantuña <aayalacantuna@cifpfbmoll.eu>
2025-11-13 19:56:41 +0100

    Activity uploaded

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hellotoast;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
+
+/*
+* RecyclerView
+* */
