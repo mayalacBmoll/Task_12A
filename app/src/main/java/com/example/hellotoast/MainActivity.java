a9ac61af5be8b160dba0bed51f5eb96f7e39d30b
Aaron Mateo Ayala Cantuña <aayalacantuna@cifpfbmoll.eu>
2025-11-13 20:23:15 +0100

    Changes

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,18 +3,46 @@ package com.example.hellotoast;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnToast;
+    private Button btnCount;
+    private TextView tvCount;
+
+    private int counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnToast = findViewById(R.id.btnToast);
+        btnCount = findViewById(R.id.btnCount);
+        tvCount = findViewById(R.id.tvCount);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello Toast!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                tvCount.setText(String.valueOf(counter));
+            }
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
 
-
-/*
-* RecyclerView
-* */
